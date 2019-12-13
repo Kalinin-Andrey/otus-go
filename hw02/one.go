f58012318ed0df7b910c73a26ed09a27f8dfb07e
Andrey Kalinin <ay.kalinin@sigma-it.ru>
2019-12-13 16:36:07 +0300

    hw02

---
+++ hw02/one.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func SimpleUnpackString( code string ) (string, error) {
+	var res string
+	codeRunes := []rune(code)
+
+	for i:=0; i < len(codeRunes); i++ {
+		var s string
+
+		if ('a' <= codeRunes[i] && codeRunes[i] <= 'z') || ('A' <= codeRunes[i] && codeRunes[i] <= 'Z') {
+			s = string(codeRunes[i])
+		} else if codeRunes[i] == '\\' {
+			s = string(codeRunes[i+1])
+			i++
+		} else {
+			return "", fmt.Errorf("Symbol N%v must be a valid letter, \"%v\" was givven.", i, codeRunes[i])
+		}
+		n := 1
+		if ('0' <= codeRunes[i + 1] && codeRunes[i + 1] <= '9') {
+			n = int(codeRunes[i + 1])
+			i++
+		}
+		res += strings.Repeat(s, n)
+	}
+	return res, nil
+}
---
+++ hw02/one_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+type TestCase struct {
+	In string
+	Out string
+	IsError bool
+}
+
+var TestCases []TestCase = []TestCase{
+	TestCase{
+		In: "a4bc2d5e",
+		Out: "aaaabccddddde",
+	},
+	TestCase{
+		In: "abcd",
+		Out: "abcd",
+	},
+	TestCase{
+		In: "45",
+		Out: "",
+		IsError: true,
+	},
+	TestCase{
+		In: `qwe\4\5`,
+		Out: "qwe45",
+	},
+	TestCase{
+		In: `qwe\45`,
+		Out: "qwe44444",
+	},
+	TestCase{
+		In: `qwe\\5`,
+		Out: `qwe\\\\\`,
+	},
+}
+
+func TestSimpleUnpackString(t *testing.T) {
+
+	for _, c := range TestCases {
+		res, err := SimpleUnpackString(c.In)
+		require.Equal(t, c.IsError, err != nil, "error not match")
+		require.Equal(t, c.Out, res, "result not match")
+	}
+}
