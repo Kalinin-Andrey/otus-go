146bb64a7b5d928c8d4a5c13f5eba8288e90aff2
Andrey Kalinin <kalinin.andrey1@mail.ru>
2019-12-15 11:19:15 +0300

    hw02 refactoring

--- hw02/simple_unpack_string.go
+++ hw02/simple_unpack_string.go
@@ -3,31 +3,34 @@ package hw02
 import (
 	"fmt"
 	"strings"
+	"unicode"
 )
 
 func SimpleUnpackString( code string ) (string, error) {
-	var res string
+	var b strings.Builder
 	codeRunes := []rune(code)
 
 	for i := 0; i < len(codeRunes); i++ {
 		var s string
 
-		if ('a' <= codeRunes[i] && codeRunes[i] <= 'z') || ('A' <= codeRunes[i] && codeRunes[i] <= 'Z') {
+		switch {
+		case unicode.IsLetter(codeRunes[i]):
 			s = string(codeRunes[i])
-		} else if codeRunes[i] == '\\' {
+		case codeRunes[i] == '\\':
 			s = string(codeRunes[i+1])
 			i++
-		} else {
+		default:
 			return "", fmt.Errorf("Symbol N%v must be a valid letter, \"%v\" was givven.", i, codeRunes[i])
 		}
 		n := 1
+
 		if i + 1 < len(codeRunes) {
-			if '0' <= codeRunes[i + 1] && codeRunes[i + 1] <= '9' {
+			if unicode.IsDigit(codeRunes[i + 1]) {
 				n = int(codeRunes[i + 1] - '0')
 				i++
 			}
 		}
-		res += strings.Repeat(s, n)
+		b.WriteString(strings.Repeat(s, n))
 	}
-	return res, nil
+	return b.String(), nil
 }
