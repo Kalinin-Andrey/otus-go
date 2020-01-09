5b8ce94b4b215d6721c713c5c452d064eb5504c3
Andrey Kalinin <ay.kalinin@sigma-it.ru>
2020-01-09 16:44:35 +0300

    fix

--- hw05/goroutines_starter.go
+++ hw05/goroutines_starter.go
@@ -4,16 +4,40 @@ import (
 	"context"
 	"github.com/pkg/errors"
 	"log"
+	"strconv"
 	"sync"
 )
 
 // GoroutineData is data for goroutine starting
 type GoroutineData struct{
 	TasksChannel       chan func()error
-	IfIsFreeChannel     chan struct{}
 	IfNeed2StartChannel chan struct{}
 	ErrorsChannel       chan error
 	WaitGroup          *sync.WaitGroup
+}
+
+func stoperByErrorsQuote(ctx context.Context, errorsCh chan error, errorsQuota uint) (errorsLimit chan struct{}) {
+	errorsLimit = make(chan struct{}, 1)
+
+	go func(){
+		var errorsCount uint
+	L1:
+		for {
+			select {
+			case <-ctx.Done():
+				break L1
+			case <- errorsCh:
+				errorsCount++
+
+				if errorsCount == errorsQuota {
+					errorsLimit <- struct{}{}
+					log.Printf("error limit %v has exceed", errorsCount)
+					break L1
+				}
+			}
+		}
+	}()
+	return errorsLimit
 }
 
 // Run function starts proceed of tasks in N gorutines with M maximum possible errors
@@ -23,40 +47,34 @@ func Run(tasks []func()error, N int, M int) (err error) {
 		return errors.New("N and M params mast be more then 0")
 	}
 
+	if N == 0 && len(tasks) > 0 {
+		return errors.New("there is no worker for given " + strconv.Itoa(len(tasks)) + " tasks")
+	}
+
 	if N == 0 || M == 0 || len(tasks) == 0 {
 		return nil
 	}
 
-	ctx, finish		:= context.WithCancel(context.Background())
-	dataSlice, wg, errorsCh	:= goroutinesStarter(ctx, uint(N), uint(M), len(tasks))
-	taskNum			:= 0
+	ctx, finish := context.WithCancel(context.Background())
+	tasksCh, wg, errorsCh := goroutinesStarter(ctx, uint(N), uint(M), len(tasks))
+	defer close(errorsCh)
+	errorsLimit := stoperByErrorsQuote(ctx, errorsCh, uint(M))
 
-	for i := 0; ; {
-
-		if len(errorsCh) == M {
+L1:
+	for _, task := range tasks {
+		select {
+		case <- errorsLimit:
 			err = errors.New("error limit exceeded")
-			log.Print("error limit exceeded")
-			break
-		}
-
-		if taskNum == len(tasks) {
-			log.Print("tasks are over")
-			break
-		}
-		_, ok := <- (*dataSlice)[i].IfIsFreeChannel
-
-		if ok {
-			(*dataSlice)[i].TasksChannel <- tasks[taskNum]
-			taskNum++
-		}
-
-		i++
-		if i == N {
-			i = 0
+			break L1
+		case tasksCh <- task:
 		}
 	}
-	log.Print("send finish signal")
+
+	if err == nil {
+		log.Print("tasks are over")
+	}
 	finish()
+	log.Print("send finish signal")
 	wg.Wait()
 	//	check again
 	if err == nil && len(errorsCh) == M {
@@ -67,13 +85,15 @@ func Run(tasks []func()error, N int, M int) (err error) {
 }
 
 // GorutinesStarter starts proceed of tasks in N gorutines with M maximum possible errors
-func goroutinesStarter(ctx context.Context, goroutinesQuota uint, errorsQuota uint, tasksNum int) (dataSlice *[]GoroutineData, wg *sync.WaitGroup, errorsCh chan error) {
-	dataSlice	= getGoroutineDataSlice(goroutinesQuota, tasksNum)
+func goroutinesStarter(ctx context.Context, goroutinesQuota uint, errorsQuota uint, tasksNum int) (tasksCh chan func() error, wg *sync.WaitGroup, errorsCh chan error) {
+	dataSlice	:= getGoroutineDataSlice(goroutinesQuota, tasksNum)
+	tasksCh     = make(chan func() error)
 	errorsCh	= make(chan error, errorsQuota)
 	wg 			= &sync.WaitGroup{}
+	wg.Add(1)
 
 	go func() {
-		defer closeTasksChannels(dataSlice)
+		defer close(tasksCh)
 		log.Print("funcs start: ")
 L1:
 		for i := uint(0); ; {
@@ -82,9 +102,9 @@ L1:
 				break L1
 			case <- (*dataSlice)[i].IfNeed2StartChannel:
 				wg.Add(1)
+				(*dataSlice)[i].TasksChannel = tasksCh
 				(*dataSlice)[i].ErrorsChannel = errorsCh
 				(*dataSlice)[i].WaitGroup = wg
-				(*dataSlice)[i].IfIsFreeChannel <- struct{}{}
 				go funcProceed(&(*dataSlice)[i])
 			default:
 			}
@@ -95,8 +115,9 @@ L1:
 			}
 		}
 		log.Print("goroutinesStarter has stop")
+		wg.Done()
 	}()
-	return dataSlice, wg, errorsCh
+	return tasksCh, wg, errorsCh
 }
 
 // getGoroutineDataSlice is get slice of GoroutineData
@@ -105,22 +126,12 @@ func getGoroutineDataSlice(N uint, tasksNum int) *[]GoroutineData {
 
 	for i := uint(0); i < N; i++ {
 		data := GoroutineData{
-			TasksChannel:       make(chan func()error, tasksNum),
-			IfIsFreeChannel:     make(chan struct{}, 1),
 			IfNeed2StartChannel: make(chan struct{}, 1),
 		}
 		data.IfNeed2StartChannel <- struct{}{}
 		dataSlice = append(dataSlice, data)
 	}
 	return &dataSlice
-}
-
-// closeTasksChannels is close tasks channels for a given []GoroutineData
-func closeTasksChannels(dataSlice *[]GoroutineData) {
-	for i, data := range *dataSlice{
-		close(data.TasksChannel)
-		log.Print("Close tasks channel ", i)
-	}
 }
 
 // funcProceed proceeds a given func
@@ -140,6 +151,5 @@ func funcProceed(data *GoroutineData) {
 		if err != nil {
 			data.ErrorsChannel <- err
 		}
-		data.IfIsFreeChannel <- struct{}{}
 	}
 }
